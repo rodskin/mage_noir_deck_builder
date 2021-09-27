3530d121edc0f85eaadb3ff7019dbd5d2d720e29
Rodskin <rod.gilles@gmail.com>
2021-09-27 15:28:42 +0200

    add mana icons

---
+++ vue_app/src/components/mana_icon.vue
@@ -0,0 +1,22 @@
+<template>
+    <img class="mana_icon" :src="mana_icon" />
+</template>
+
+<script>
+export default {
+    name: 'mana_icon',
+    props: ['mana'],
+    data() {
+        return {
+            mana_icon: require('@/assets/icons/' + this.mana + '_icon.png')
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .mana_icon {
+        border-radius: 50%;
+    }
+</style>
+
