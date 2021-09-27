5626e9540166c3d9b12a592aa095fcfc025a7d0b
Rodskin <rod.gilles@gmail.com>
2021-09-27 15:52:34 +0200

    deactivate sidebar

--- vue_app/src/main.js
+++ vue_app/src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App'
 import router from './router'
-import VueSidebarMenu from 'vue-sidebar-menu'
-import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+//import VueSidebarMenu from 'vue-sidebar-menu'
+//import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
 import cardHolder from '@/components/card_holder'
+import manaIcon from '@/components/mana_icon'
 
 // DATAS
 import cards from './datas/cards.js'
@@ -12,8 +13,9 @@ import cards from './datas/cards.js'
 // APP
 const app = createApp(App)
 app.use(router)
-app.use(VueSidebarMenu)
+//app.use(VueSidebarMenu)
 app.component('cardHolder', cardHolder)
+app.component('manaIcon', manaIcon)
 app.config.globalProperties.$cards = cards
 
 app.mount('#app')
