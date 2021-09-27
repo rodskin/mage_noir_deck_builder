a4030257f9364149535180c6dffcc7fe35f649de
Rodskin <rod.gilles@gmail.com>
2021-09-27 15:42:31 +0200

    add sidebar

--- vue_app/src/main.js
+++ vue_app/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App'
 import router from './router'
+import VueSidebarMenu from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
 import cardHolder from '@/components/card_holder'
 
@@ -10,6 +12,7 @@ import cards from './datas/cards.js'
 // APP
 const app = createApp(App)
 app.use(router)
+app.use(VueSidebarMenu)
 app.component('cardHolder', cardHolder)
 app.config.globalProperties.$cards = cards
 
