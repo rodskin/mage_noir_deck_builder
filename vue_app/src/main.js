caea3b076d0002b643ff54b77969a9d3aa904368
Rodskin <rod.gilles@gmail.com>
2021-09-27 17:47:02 +0200

    add scss

--- vue_app/src/main.js
+++ vue_app/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 //import VueSidebarMenu from 'vue-sidebar-menu'
 //import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
+// COMMON COMPONNENTS
 import cardHolder from '@/components/card_holder'
 import manaIcon from '@/components/mana_icon'
 
@@ -11,6 +12,21 @@ import manaIcon from '@/components/mana_icon'
 import cards from './datas/cards.js'
 import currentDeck from './datas/current_deck.js'
 import ingredients from './datas/ingredients.js'
+
+// CSS
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+require('@/assets/scss/_root.scss')
+
+// FONT AWECOME ICONS
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+library.add(faBook);
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
+library.add(faExclamationTriangle);
+import { faUndoAlt } from "@fortawesome/free-solid-svg-icons";
+library.add(faUndoAlt);
 
 // APP
 const app = createApp(App)
